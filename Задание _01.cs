11661493ed85a9dc067b6fbebce46b9d9df7e2b6
zhannulka <124399184+zhannulka@users.noreply.github.com>
2023-02-24 20:42:29 +0300

    Add files via upload

---
+++ Задание _01.cs
@@ -0,0 +1,31 @@
+﻿// // // Задача 19
+// // // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// // // 14212 -> нет
+// // // 12821 -> да
+// // // 23432 -> 
+
+
+Console.WriteLine("Введите число: ");
+int number= Convert.ToInt32(Console.ReadLine());
+if(number <= 100000 && number > 10000)
+{
+  int remineder, newnamber = 0;
+int temp = number;
+while (number > 0)
+{
+remineder = number %  10;
+newnamber = newnamber * 10  + remineder;
+number = number / 10;
+}
+if (temp == newnamber)
+ {
+Console.WriteLine($"Число {temp} палиндром ");
+ }
+else
+{
+Console.WriteLine($"Число {temp} не палиндром ");}}
+    
+else
+{
+  Console.WriteLine ("Вы ввели не пятизначное число, пожалуйста повторите ввод");}
+
---
+++ Задание_02.cs
@@ -0,0 +1,25 @@
+﻿// Задача 21
+// Напишите программу, которая принимает 
+// на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Введите y для точки А: ");
+int yA  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x для точки А: ");
+int xA  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z для точки А: ");
+int zA  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y для точки B: ");
+int yB  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x для точки B: ");
+int xB  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z для точки B: ");
+int zB  = Convert.ToInt32(Console.ReadLine());
+
+ double distance = Math.Round(Math.Pow((Math.Pow(xA - xB, 2) +
+                         Math.Pow(yA - yB, 2) +
+                         Math.Pow(zA - zB, 2) *
+                                   1.0), 0.5), 2);
+    Console.WriteLine("Расстяние между двумя точками = " + distance);
+
