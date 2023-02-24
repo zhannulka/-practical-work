de34bcd98bd47c40e42bec8d6fe42ff333c50060
zhannulka <124399184+zhannulka@users.noreply.github.com>
2023-02-24 20:37:06 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
-﻿
-// Задача 23
-// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
+﻿// Задача 21
+// Напишите программу, которая принимает 
+// на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= number; i++) 
-Console.Write($"{Math.Pow(i, 3)}, ");
-Console.WriteLine(" ");
+Console.Write("Введите y для точки А: ");
+int yA  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x для точки А: ");
+int xA  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z для точки А: ");
+int zA  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y для точки B: ");
+int yB  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x для точки B: ");
+int xB  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z для точки B: ");
+int zB  = Convert.ToInt32(Console.ReadLine());
+
+ double distance = Math.Round(Math.Pow((Math.Pow(xA - xB, 2) +
+                         Math.Pow(yA - yB, 2) +
+                         Math.Pow(zA - zB, 2) *
+                                   1.0), 0.5), 2);
+    Console.WriteLine("Расстяние между двумя точками = " + distance);
+
